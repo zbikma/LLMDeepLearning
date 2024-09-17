3e9787725441a346f641f9f2a17bdfad9592a4f1
zbikma <z.mazadi@gmail.com>
2024-09-17 01:51:18 -0400

    fixed merge conflicts

--- CodeGenAgent/main.py
+++ CodeGenAgent/main.py
@@ -2,7 +2,7 @@ from llama_index.llms.ollama import Ollama
 # lib lamaparse for loading unstructured , semi-structured files like pdf file in this example
 from llama_parse import LlamaParse
 from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, PromptTemplate
-from llama
+from llama_index.core.embeddings import resolve_embed_model
 llm= Ollama(model="mistral",request_timeout=30.0)
 
 result = llm.complete("hello world")
