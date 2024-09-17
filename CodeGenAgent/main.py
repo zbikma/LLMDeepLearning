628192f96aacf6b3ff8753f36cd0de8c4ddfc10e
zbikma <z.mazadi@gmail.com>
2024-09-17 01:38:25 -0400

    code generator agent

---
+++ CodeGenAgent/main.py
@@ -0,0 +1,9 @@
+from llama_index.llms.ollama import Ollama
+# lib lamaparse for loading unstructured , semi-structured files like pdf file in this example
+from llama_parse import LlamaParse
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, PromptTemplate
+from llama
+llm= Ollama(model="mistral",request_timeout=30.0)
+
+result = llm.complete("hello world")
+print(result)
---
+++ CodeGenAgent/readme.md
@@ -0,0 +1,2 @@
+## Code Generator Agent using LamaIndex
+this is an implementation of this tutorial[https://www.youtube.com/watch?v=JLmI0GJuGlY&t=574s]. for the requirements to work you would need Python version 3.9
