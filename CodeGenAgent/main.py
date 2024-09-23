452cc1786ee59f331fe7362a84873936a497eaca
zbikma <z.mazadi@gmail.com>
2024-09-22 22:14:38 -0400

    more logic to create a chain using the result and serializing to json using pydantic

--- CodeGenAgent/main.py
+++ CodeGenAgent/main.py
@@ -1,4 +1,6 @@
 from llama_index.llms.ollama import Ollama
+import ast
+import os
 from llama_index.core.tools import QueryEngineTool, ToolMetadata
 from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
@@ -7,7 +9,11 @@ from llama_parse import LlamaParse
 from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, PromptTemplate
 from llama_index.core.embeddings import resolve_embed_model
 from llama_index.core.agent import ReActAgent 
-from prompts import context
+from prompts import context,code_parser_template
+from codereader import code_reader
+from pydantic import BaseModel
+from llama_index.core.output_parsers import PydanticOutputParser
+from llama_index.core.query_pipeline import QueryPipeline
 
 llm= Ollama(model="mistral",request_timeout=3600.0)
 # we want to parse our file specially in case of unstructured data like pdf files so we can better query it 
@@ -29,16 +35,48 @@ tools = [
             name="api_documentation",
             description="this gives documentation about code for an API. Use this for reading docs for the API",
         ),
-    )
+    ),
+    code_reader,
 ]
 # codellama is a model that is designed for code generation instead of just question and naswer
 code_llm = Ollama(model="codellama",request_timeout=3600.0)
 agent = ReActAgent.from_tools(tools, llm=code_llm, verbose=True, context=context)
-while(prompt:= input("Enter a prompt (q to quit):")) != "q":
+class CodeOutPut(BaseModel):
+    code: str
+    description : str
+    filename : str
+
+parser = PydanticOutputParser(CodeOutPut)
+json_prompt_str = parser.format(code_parser_template)
+json_prompt_temp= PromptTemplate(json_prompt_str)
+output_pipeline = QueryPipeline(chain=[json_prompt_temp,llm])
+while (prompt := input("Enter a prompt (q to quit): ")) != "q":
+    retries = 0
+
+    while retries < 3:
+        try:
+            result = agent.query(prompt)
+            print("Agent response:", result)
+            next_result = output_pipeline.run(response=result)
+            cleaned_json = ast.literal_eval(str(next_result).replace("assistant:", ""))
+            break
+        except Exception as e:
+            retries += 1
+            print(f"Error occured, retry #{retries}:", e)
+
+    if retries >= 3:
+        print("Unable to process request, try again...")
+        continue
+
+    print("Code generated")
+    print(cleaned_json["code"])
+    print("\n\nDesciption:", cleaned_json["description"])
+
+    filename = cleaned_json["filename"]
+
     try:
-
-        result= agent.query(prompt)
-        print(result)
-    except Exception as e:
-        print(f"there is an issue that i cannot act as usuall: {e}")
-
+        with open(os.path.join("output", filename), "w") as f:
+            f.write(cleaned_json["code"])
+        print("Saved file", filename)
+    except:
+        print("Error saving file...")
