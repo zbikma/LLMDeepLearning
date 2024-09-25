c2394ca231e9b09b27e314a49a8cb3e66f36734d
zbikma <z.mazadi@gmail.com>
2024-09-25 18:25:37 -0400

    Update crewai/utils.py
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>

--- crewai/utils.py
+++ crewai/utils.py
@@ -20,21 +20,20 @@ def get_serper_api_key():
 # break line every 80 characters if line is longer than 80 characters
 # don't break in the middle of a word
 def pretty_print_result(result):
-  parsed_result = []
-  for line in result.split('\n'):
-      if len(line) > 80:
-          words = line.split(' ')
-          new_line = ''
-          for word in words:
-              if len(new_line) + len(word) + 1 > 80:
-                  parsed_result.append(new_line)
-                  new_line = word
-              else:
-                  if new_line == '':
-                      new_line = word
-                  else:
-                      new_line += ' ' + word
-          parsed_result.append(new_line)
-      else:
-          parsed_result.append(line)
-  return "\n".join(parsed_result)
+    parsed_result = []
+    for line in result.split('\n'):
+        if len(line) > 80:
+            words = line.split(' ')
+            new_line = ''
+            for word in words:
+                if len(new_line) + len(word) > 79:
+                    parsed_result.append(new_line)
+                    new_line = word
+                elif new_line == '':
+                    new_line = word
+                else:
+                    new_line += f' {word}'
+            parsed_result.append(new_line)
+        else:
+            parsed_result.append(line)
+    return "\n".join(parsed_result)
